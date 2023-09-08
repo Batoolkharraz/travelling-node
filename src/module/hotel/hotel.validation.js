8880415f04a51705d16ec133063e642fa7b2e97a
Batoolkharraz <99608072+Batoolkharraz@users.noreply.github.com>
2023-09-08 17:25:57 +0300

    edit validation

--- src/module/hotel/hotel.validation.js
+++ src/module/hotel/hotel.validation.js
@@ -1,28 +1,28 @@
 import joi from "joi";
 import { generalFeilds } from "../../MiddleWare/validation.js";
 
-export const createHotelreStoreHotel=joi.object({
+export const createHotel=joi.object({
     name:joi.string().min(2).max(20).required(),
     description:joi.string().min(1).max(500).required(),
     rate:joi.number().positive().min(0).max(5),
 }).required();
 
-export const updateHotelreStoreHotel=joi.object({
+export const updateHotel=joi.object({
     countryId:generalFeilds.id,
     name:joi.string().min(2).max(20),
     description:joi.string().min(1).max(500),
     rate:joi.number().positive().min(0).max(5),
 }).required();
 
-export const getHotelreStoreHotel = joi.object({
+export const getHotel = joi.object({
     countryId:generalFeilds.id,
 }).required();
 
-export const deleteHotelreStoreHotel = joi.object({
+export const deleteHotel = joi.object({
     countryId:generalFeilds.id,
 }).required();
 
-export const softDeleteHotelreStoreHotel = joi.object({
+export const softDeleteHotel = joi.object({
     countryId:generalFeilds.id,
 }).required();
 
